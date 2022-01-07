484a4b292bd60a6439a849733bf3813810d66ebe
twlee <twlee@foodtechkorea.com>
2022-01-07 15:16:09 +0900

    spring security config에 유저정보 추가

--- src/main/java/kr/taeu/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/kr/taeu/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package kr.taeu.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,5 +19,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .formLogin()
                 .and()
             .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("twlee").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
